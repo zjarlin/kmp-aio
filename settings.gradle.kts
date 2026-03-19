0ee4ff78d0808a92bb1ca5deb079f7884213417d
zjarlin <zjarlin@outlook.com>
2026-03-19 22:15:00 +0800

    change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = rootDir.name
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-includeBuild("../addzero-lib-jvm")
+//includeBuild("../addzero-lib-jvm")
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("site.addzero.gradle.plugin.modules-buddy") version "2026.01.11"
+    id("site.addzero.gradle.plugin.modules-buddy") version "+"
     id("site.addzero.gradle.plugin.repo-buddy") version "2025.11.30"
     id("site.addzero.gradle.plugin.addzero-git-dependency") version "2026.02.25"
 
