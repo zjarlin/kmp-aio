271c07e6075f5f662c6799bece3494c06ffb82f0
zjarlin <zjarlin@outlook.com>
2026-04-07 15:11:24 +0800

    Remove embedded Koin scan repro

--- repro/koin-commonmain-wide-scan/feature-alpha/src/commonMain/kotlin/site/addzero/repro/feature/alpha/AlphaRepository.kt
+++
@@ -1,8 +0,0 @@
-package site.addzero.repro.feature.alpha
-
-import org.koin.core.annotation.Single
-
-@Single
-class AlphaRepository {
-    fun label(): String = "alpha"
-}
--- repro/koin-commonmain-wide-scan/settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "koin-commonmain-wide-scan"
-
-include(":app")
-include(":feature-alpha")
-include(":feature-beta")
-include(":feature-gamma")
-include(":feature-provider")
